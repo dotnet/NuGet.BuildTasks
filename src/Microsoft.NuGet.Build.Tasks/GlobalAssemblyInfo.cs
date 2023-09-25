3942c53c91bc641294e1ae5c83af144d0207a044
Tom Meschter (from Dev Box) <tomescht@microsoft.com>
2023-09-25 13:29:24 -0700

    Add DefaultDllImportSearchPaths
    Related to #1853770.
    
    Use the `DefaultDllImportSearchPaths` attribute to ensure that P/Invokes (if any) from within this assembly only load native libraries from trusted and safe paths.

---
+++ src/Microsoft.NuGet.Build.Tasks/GlobalAssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.InteropServices;
+
+// Ensure P/Invokes from within this assembly only load libraries from trusted and safe paths.
+[assembly: DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
