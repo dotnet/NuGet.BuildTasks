e88c831043bcce9149176ff4dff36fcfce74381c
Tom Meschter (from Dev Box) <tomescht@microsoft.com>
2023-09-25 13:33:35 -0700

    Add header

--- src/Microsoft.NuGet.Build.Tasks/GlobalAssemblyInfo.cs
+++ src/Microsoft.NuGet.Build.Tasks/GlobalAssemblyInfo.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved. 
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
+
 using System.Runtime.InteropServices;
 
 // Ensure P/Invokes from within this assembly only load libraries from trusted and safe paths.
